92a52d41003c66ba8cb306a266ac91f3ec546fde
Ambika Eshwar <ambika@rosalogia.me>
2022-03-08 17:02:02 -0600

    Fixed issue with recv lock

--- lib/client.ml
+++ lib/client.ml
@@ -19,23 +19,14 @@ let send_to client payload peer =
   let%lwt _ = sendto !client.socket payload 0 len [] addr in
   Lwt.return ()
 
-  (* Warning: potentially bad design
-  While this function locks the receive mutex, it does
-  NOT unlock it. Unlocking it would mean that _any_
-  following call to recv_next would be able to read
-  from the socket next. Instead, calls to read_size_of_incoming
-  MUST be followed by the completion of an already initiated
-  call to recv_next. *)
-let read_size_of_incoming client =
-  let open Lwt_unix in
-  let%lwt () = Lwt_mutex.lock !client.recv_mutex in
-  let size_buffer = Bytes.create 8 in
-  let%lwt _ = recvfrom !client.socket size_buffer 0 8 [MSG_PEEK] in
-  let len = (Encoding.read_size_header size_buffer) + 8 in
-  Lwt.return len
-
 let recv_next client =
-  let%lwt msg_size = read_size_of_incoming client in
+  let open Lwt_unix in
+  (* Peek at the first 8 bytes of the incoming datagram
+  to read the Bin_prot size header. *)
+  let size_buffer = Bytes.create 8 in
+  let%lwt () = Lwt_mutex.lock !client.recv_mutex in
+  let%lwt _ = recvfrom !client.socket size_buffer 0 8 [MSG_PEEK] in
+  let msg_size = (Encoding.read_size_header size_buffer) + 8 in
   let msg_buff = Bytes.create msg_size in
   let%lwt (_, addr) = recvfrom !client.socket msg_buff 0 msg_size [] in
   Lwt_mutex.unlock !client.recv_mutex;
