e9c9487579df246cd97a5a8403584a958917fdd1
gsebil08 <gauthier@marigold.dev>
2022-03-09 14:59:27 +0000

    ping pong test

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ A platform agnostic OCaml library for P2P communications using UDP and Bin_prot.
 
 Make sure you have [`esy`](https://esy.sh/) installed.
 
-```
+```shell script
 # Build
 $ esy
 # Test
-$ esy x dune test
+$ esy test
 ```
--- test/pollinate_tests.ml
+++ test/pollinate_tests.ml
@@ -5,14 +5,16 @@ module Client_tests = struct
   open Pollinate
 
   type request =
+    | Ping
     | Get
     | Insert of string
-  [@@deriving bin_io]
+  [@@deriving bin_io, show { with_path = false }]
   type response =
+    | Pong
     | List    of string list
     | Success of string
     | Error   of string
-  [@@deriving bin_io]
+  [@@deriving bin_io, show { with_path = false }]
 
   type state = string list
 
@@ -20,21 +22,23 @@ module Client_tests = struct
     let request = Util.Encoding.unpack bin_read_request request in
     let response =
       match request with
+      | Ping -> Pong
       | Get -> List !state
       | Insert s ->
         state := s :: !state;
         Success "Successfully added value to state" in
     Util.Encoding.pack bin_writer_response response
 
+  let client_a =
+    Lwt_main.run @@ Client.init ~state:["test1"] ~msg_handler ("127.0.0.1", 3000)
+  let client_b =
+    Lwt_main.run @@ Client.init ~state:["test2"] ~msg_handler ("127.0.0.1", 3005)
+
   (* Initializes two peers and has each one request the state
      of the other, returning the first element in the response of each *)
   let trade_messages () =
-    let%lwt client_a =
-      Client.init ~state:["test1"] ~msg_handler ("127.0.0.1", 3000) in
     let peer_a = Client.peer_from !client_a in
 
-    let%lwt client_b =
-      Client.init ~state:["test2"] ~msg_handler ("127.0.0.1", 3005) in
     let peer_b = Client.peer_from !client_b in
 
     let req = Util.Encoding.pack bin_writer_request Get in
@@ -55,11 +59,29 @@ module Client_tests = struct
       | _ -> failwith "Incorrect response" in
 
     Lwt.return (res_a, res_b)
+  let ping_pong () =
+    let peer_b = Client.peer_from !client_b in
+
+    let ping = Util.Encoding.pack bin_writer_request Ping in
+
+    let%lwt () = Client.send_to client_a ping peer_b in
+    let%lwt pong, _ = Client.recv_next client_a in
+
+    let pong = Util.Encoding.unpack bin_read_response pong in
+
+    let pong =
+      match pong with
+      | Pong -> show_response Pong
+      | _ -> failwith "Incorrect response" in
+
+    Lwt.return pong
 end
 
 let test_trade_messages _ () =
   Client_tests.trade_messages ()
   >|= Alcotest.(check (pair string string)) "test2 and test1" ("test2", "test1")
+let test_ping_pong _ () =
+  Client_tests.ping_pong () >|= Alcotest.(check string) "Ping pong" "Pong"
 
 let () =
   Lwt_main.run
@@ -68,5 +90,6 @@ let () =
          ( "communication",
            [
              Alcotest_lwt.test_case "Trading Messages" `Quick test_trade_messages;
+             Alcotest_lwt.test_case "Ping pong" `Quick test_ping_pong;
            ] );
        ]
