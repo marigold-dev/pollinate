657c1177bfa9e38e117b9a38f45b943171117ba5
gsebil08 <gauthier@marigold.dev>
2022-03-11 15:19:07 +0000

    swim(initSwimProtocol): re-work + fix ocamlformat

--- lib/peer.mli
+++ lib/peer.mli
@@ -3,22 +3,25 @@ type state =
   | Suspicious
   | Faulty
 
+(** Our representation of a Peer *)
 type t = {
   address : string;
   port : int;
   known_peers : t list;
   state : state;
 }
-(** Our representation of a Peer *)
 
-val from_sockaddr : Unix.sockaddr -> t
 (** Obtain a Pollinate.Peer from a Unix.sockaddr *)
+val from_sockaddr : Unix.sockaddr -> t
 
-val to_sockaddr : t -> Unix.sockaddr
 (** Obtain a Unix.sockaddr from a Pollinate.Peer *)
+val to_sockaddr : t -> Unix.sockaddr
 
+(** Add the given peer to the existing peer *)
 val add_peer : t -> t -> t
 
+(** Basic Knuth shuffle for the know_peers list*)
 val knuth_shuffle : t list -> t list
 
+(** Randomly pick one know_peer from the list of the given peer*)
 val pick_random_member : t -> t
