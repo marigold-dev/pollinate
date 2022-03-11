0bfc204c96f3a4f4efdbec8cb6baef37d2e085b8
gsebil08 <gauthier@marigold.dev>
2022-03-11 16:09:11 +0000

    swim(initSwimProtocol): add comments to .mli

--- lib/peer.mli
+++ lib/peer.mli
@@ -1,9 +1,13 @@
+(** Types and functions pertaining to peers in a P2P application *)
+
+(** Sum type holding all the available values for peer state *)
 type state =
   | Alive
   | Suspicious
   | Faulty
 
-(** Our representation of a Peer *)
+(** Our representation of a Peer
+  See Client.peer_from to construct one *)
 type t = {
   address : string;
   port : int;
@@ -17,8 +21,14 @@ val from_sockaddr : Unix.sockaddr -> t
 (** Obtain a Unix.sockaddr from a Pollinate.Peer *)
 val to_sockaddr : t -> Unix.sockaddr
 
+(** Each peer have a list of known peers
+  this function add the provided peer to the previous list
+  of the given peer *)
 val add_peer : t -> t -> t
 
+(** Following SWIM protocol, the list mut be randomized after adding a peer *)
 val knuth_shuffle : t list -> t list
 
+(** Following SWIM protocol,
+  the pick of a peer to which send the Ping message must be randomly chosen*)
 val pick_random_member : t -> t
