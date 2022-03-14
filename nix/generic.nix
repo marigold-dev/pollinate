f50d7a9d2bf2164a788623ecc63bd758f1d1752c
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-14 09:31:03 +0100

    Init nix setup

---
+++ nix/generic.nix
@@ -0,0 +1,34 @@
+{ pkgs, stdenv, lib, ocamlPackages, static ? false, doCheck }:
+
+with ocamlPackages;
+rec {
+  pollinate = buildDunePackage {
+    pname = "pollinate";
+    version = "0.1.0";
+
+    src = lib.filterGitSource {
+      src = ./..;
+      dirs = [ "src" ];
+      files = [ "dune-project" "pollinate.opam" ];
+    };
+
+    checkInputs = [
+      alcotest
+      alcotest-lwt
+    ];
+
+    propagatedBuildInputs = [
+      lwt
+      lwt_ppx
+      bin_prot
+      ppx_bin_prot
+      ppx_deriving
+    ];
+
+    inherit doCheck;
+
+    meta = {
+      description = "A platform agnostic library for P2P communications using UDP and Bin_prot";
+    };
+  };
+}
