96829614dd400bbd284b99325e252d693131a258
Ambika Eshwar <ambika@rosalogia.me>
2022-03-16 09:58:39 -0500

    Updated thread-safe queue library to use condition variables

--- lib/tqueue.ml
+++ lib/tqueue.ml
@@ -1,141 +1,65 @@
 type 'a t = {
   queue : 'a Queue.t;
   lock : Lwt_mutex.t;
-  mailbox : 'a Lwt_mvar.t;
+  has_elt : unit Lwt_condition.t;
 }
 
 let create () =
   let queue = Queue.create () in
   let lock = Lwt_mutex.create () in
-  let mailbox = Lwt_mvar.create_empty () in
-  { queue; lock; mailbox }
+  let has_elt = Lwt_condition.create () in
+  { queue; lock; has_elt }
 
-let add x { queue; lock; mailbox } =
+let add x { queue; lock; has_elt } =
   Lwt_mutex.with_lock lock (fun () ->
-      if Lwt_mvar.is_empty mailbox then
-        Lwt_mvar.put mailbox x
-      else
-        Lwt.return (Queue.add x queue))
+      Queue.add x queue;
+      let%lwt () =
+        if Queue.length queue = 1 then (
+          Lwt_condition.signal has_elt ();
+          Lwt.return ())
+        else
+          Lwt.return () in
+      Lwt.return ())
 
 let push = add
 
-let fill_mailbox queue mailbox =
-  if Lwt_mvar.is_empty mailbox then
-    match Queue.take_opt queue with
-    | None -> Lwt.return ()
-    | Some value -> Lwt_mvar.put mailbox value
-  else
-    Lwt.return ()
+let take { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.take queue))
 
-let take_exn { queue; lock; mailbox } =
+let take_opt { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.take_opt queue))
+
+let wait_to_take { queue; lock; has_elt } =
   Lwt_mutex.with_lock lock (fun () ->
-      let value_opt = Lwt_mvar.take_available mailbox in
-      let value =
-        match value_opt with
-        | None -> raise Queue.Empty
-        | Some value -> Lwt.return value in
-      let%lwt () = fill_mailbox queue mailbox in
-      value)
+      let%lwt () =
+        if Queue.is_empty queue then
+          Lwt_condition.wait ~mutex:lock has_elt
+        else
+          Lwt.return () in
+      Lwt.return (Queue.take queue))
 
-let take_opt { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let value = Lwt.return (Lwt_mvar.take_available mailbox) in
-      let%lwt () = fill_mailbox queue mailbox in
-      value)
+let pop = take
 
-let take_block { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let value = Lwt_mvar.take mailbox in
-      let%lwt () = fill_mailbox queue mailbox in
-      value)
+let peek { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.peek queue))
 
-let pop = take_exn
-
-let peek { lock; mailbox; _ } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let value_opt = Lwt_mvar.take_available mailbox in
-      let value =
-        match value_opt with
-        | None -> raise Queue.Empty
-        | Some value ->
-          let%lwt () = Lwt_mvar.put mailbox value in
-          Lwt.return value in
-      value)
-
-let peek_opt { lock; mailbox; _ } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let value_opt = Lwt_mvar.take_available mailbox in
-      let value =
-        match value_opt with
-        | None -> Lwt.return None
-        | Some value ->
-          let%lwt () = Lwt_mvar.put mailbox value in
-          Lwt.return (Some value) in
-      value)
+let peek_opt { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.peek_opt queue))
 
 let top = peek
 
-let clear { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      ignore (Lwt_mvar.take_available mailbox);
-      Lwt.return (Queue.clear queue))
+let clear { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.clear queue))
 
-let copy { queue; lock; mailbox } =
+let copy { queue; lock; _ } =
   Lwt_mutex.with_lock lock (fun () ->
-      let queue' = Queue.copy queue in
+      let queue' = queue in
       let lock' = Lwt_mutex.create () in
-      let mailbox' =
-        match Lwt_mvar.take_available mailbox with
-        | None -> Lwt_mvar.create_empty ()
-        | Some value -> Lwt_mvar.create value in
-      Lwt.return { queue = queue'; lock = lock'; mailbox = mailbox' })
+      let has_elt' = Lwt_condition.create () in
+      Lwt.return { queue = queue'; lock = lock'; has_elt = has_elt' })
 
-let is_empty { lock; mailbox; _ } =
-  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Lwt_mvar.is_empty mailbox))
+let is_empty { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.is_empty queue))
 
-let length { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let l = Queue.length queue + if Lwt_mvar.is_empty mailbox then 0 else 1 in
-      Lwt.return l)
-
-let iter f { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      let mb_value = Lwt_mvar.take_available mailbox in
-      let first_result =
-        match mb_value with
-        | None -> Lwt.return None
-        | Some value ->
-          let%lwt () = f value in
-          Lwt.return (Some ()) in
-      match%lwt first_result with
-      | None -> Lwt.return ()
-      | Some () ->
-        let q = Queue.to_seq queue in
-        let _v = Seq.map f q in
-        Lwt.return ())
-
-let fold f accu { queue; lock; mailbox } =
-  Lwt_mutex.with_lock lock (fun () ->
-      match Lwt_mvar.take_available mailbox with
-      | None -> Lwt.return accu
-      | Some value ->
-        queue
-        |> Queue.to_seq
-        |> Seq.cons value
-        |> Seq.fold_left f accu
-        |> Lwt.return)
-
-(* let transfer q1 q2 =
-     let%lwt () = iter (fun x -> add x q2) q1 in
-     clear q1
-
-   let to_seq { queue; lock } =
-     Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.to_seq queue))
-
-   let add_seq { queue; lock } seq =
-     Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.add_seq queue seq))
-
-   let of_seq seq =
-     let queue = Queue.of_seq seq in
-     let lock = Lwt_mutex.create () in
-     { queue; lock } *)
+let length { queue; lock; _ } =
+  Lwt_mutex.with_lock lock (fun () -> Lwt.return (Queue.length queue))
--- lib/tqueue.mli
+++ lib/tqueue.mli
@@ -1,20 +1,24 @@
 (** Thread-safe queues with the same exact interface
 as OCaml's Queue module, except any function that
 reads from or writes to a Queue returns a promise.
+Furthermore, there is a special take function for blocking
+the current thread until an element is available to take
+from the queue.
 Read the documentation for OCaml's Queue module
 for more information about the functions in this module *)
 
-(** The type of a thread-safe queue, simply consists
-of a Queue.t and a Lwt_mutex.t for protecting it. *)
+(** The type of a thread-safe queue *)
 type 'a t
 
 val create : unit -> 'a t
 val add : 'a -> 'a t -> unit Lwt.t
 val push : 'a -> 'a t -> unit Lwt.t
-val fill_mailbox : 'a Queue.t -> 'a Lwt_mvar.t -> unit Lwt.t
-val take_exn : 'a t -> 'a Lwt.t
-val take_block : 'a t -> 'a Lwt.t
+val take : 'a t -> 'a Lwt.t
 val take_opt : 'a t -> 'a option Lwt.t
+(** Blocks the current thread of execution until
+an element is in the queue, then returns a promise
+containing the element *)
+val wait_to_take : 'a t -> 'a Lwt.t
 val pop : 'a t -> 'a Lwt.t
 val peek : 'a t -> 'a Lwt.t
 val peek_opt : 'a t -> 'a option Lwt.t
@@ -23,5 +27,3 @@ val clear : 'a t -> unit Lwt.t
 val copy : 'a t -> 'a t Lwt.t
 val is_empty : 'a t -> bool Lwt.t
 val length : 'a t -> int Lwt.t
-val iter : ('a -> unit Lwt.t) -> 'a t -> unit Lwt.t
-val fold : ('a -> 'b -> 'a) -> 'a -> 'b t -> 'a Lwt.t
