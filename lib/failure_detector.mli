290a312c96b470b568602c84e20f7d7789ba39d7
Ambika Eshwar <ambika@rosalogia.me>
2022-03-22 18:35:41 -0500

    Update docs for failure detector, remove internal functions from mli and comment out tests

--- lib/failure_detector.mli
+++ lib/failure_detector.mli
@@ -1,16 +1,32 @@
-(** Messages sent by the protocol *)
+(** Fast, automatic detection and dissemination of peers dying
+or leaving the network. Runs asynchronously as part of the client
+and works to maintain a membership list of active nodes. *)
+
+(** Messages sent by the failure detector protocol *)
 type message =
   | Ping
   | Acknowledge
   | PingRequest of Address.t
 
-(** Here is the definition of the SWIM protocol *)
+(** Configurable parameters that affect various aspects of the failure
+detector *)
 type config = {
-  (* This is the global protocol period, it should be at least three times the round_trip_time *)
+  (** The period of time within which peers may be randomly chosen
+  to be pinged, and within which any peer who has been pinged must
+  respond with an acknowledgement message to continue being considered
+  alive to other nodes in the network. The protocol period should
+  be at least three times the round_trip_time. *)
   protocol_period : int;
-  (* The round trip should be around the 99th percentile *)
+ 
+  (* TODO: Implement automatic configuration/empirical determination of an ideal round-trip time *)
+  (** The amount of time a node performing a random-probe of a
+  peer will wait before asking other active peers to probe the
+  same peer. This value must be at most a third of the protocol period,
+  but it is best if it is chosen empirically. *)
   round_trip_time : int;
-  (* Number of peers to pick at each round *)
+  (** The size of 'failure detection subgroups'. In other words, the 
+  number of peers that will be asked to ping a suspicious node which
+  has failed to respond with acknowledgement during the round_trip_time. *)
   peers_to_ping : int;
 }
 
@@ -21,18 +37,15 @@ type t
 with a default state and given config *)
 val create : config -> t
 
-(** At each round, the protocol will randomly pick two peers:
+(* (** At each round, the protocol will randomly pick two peers:
   - First one: will be the sender of the Ping
   - Second one: the recipient *)
 val pick_random_peer_addresses :
-  (Address.t, Peer.t) Base.Hashtbl.t -> int -> Address.t list
+  (Address.t, Peer.t) Base.Hashtbl.t -> int -> Address.t list *)
 
-(** Send the provided message to the provided peer, using the provided Client *)
-val send_message : message -> 'a Client.t ref -> Peer.t -> unit Lwt.t
+(** Processes an incoming message bound for the failure detector *)
+val handle_payload : t -> 'a Client.t ref -> Peer.t -> message -> unit Lwt.t
 
 (** High level function, which must be run within an async thread, like:
  Lwt.async (fun () -> failure_detection t client); *)
-(** Basic random shuffle, see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle*)
-val knuth_shuffle : Address.t list -> Address.t list
-
 val failure_detection : t -> 'a Client.t ref -> 'b
