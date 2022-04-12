fe32d605b057dcf390a8d7905c99488d72f4ddee
gsebil08 <gauthier@marigold.dev>
2022-04-12 14:46:47 +0000

    add an other unit test

--- test/failure_detector_tests.ml
+++ test/failure_detector_tests.ml
@@ -22,16 +22,28 @@ let suspicion_detection () =
   let%lwt _ = SUT.suspicious_detection node_a in
   Lwt.return (Base.Hashtbl.length !node_a.peers = 0)
 
+let suspicion_detection_nothing_on_alive () =
+  let open Common.Peer in
+  let _ = add_neighbor (Pollinate.Node.Client.peer_from !node_a) peer_b in
+  let _ = SUT.update_peer_status node_a peer_b Alive in
+  let%lwt _ = SUT.suspicious_detection node_a in
+  Lwt.return (Base.Hashtbl.length !node_a.peers = 1)
+
 let test_suspicion_detection _ () =
-  suspicion_detection () >|= Alcotest.(check bool) "test2 and test1" true
+  suspicion_detection () >|= Alcotest.(check bool) "" true
+
+let test_suspicion_detection_nothing_on_alive _ () =
+  suspicion_detection_nothing_on_alive () >|= Alcotest.(check bool) "" true
 
 let () =
   Lwt_main.run
-  @@ Alcotest_lwt.run "Suspicion detecter"
+  @@ Alcotest_lwt.run "Suspicion detector"
        [
          ( "failure_detector.ml",
            [
-             Alcotest_lwt.test_case "Remove suspicious peer" `Quick
+             Alcotest_lwt.test_case "Remove Suspicious peer" `Quick
                test_suspicion_detection;
+             Alcotest_lwt.test_case "Do nothing on Alive peer" `Quick
+               test_suspicion_detection_nothing_on_alive;
            ] );
        ]
