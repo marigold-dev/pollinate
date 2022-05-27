297c0fb2c6a0ac12fa0cb77b5cc667191be5c959
Ambika Eshwar <ambika@rosalogia.me>
2022-05-27 15:06:55 -0500

    Update client to reflect changes in messages

--- lib/node/client.mli
+++ lib/node/client.mli
@@ -11,8 +11,12 @@ val address_of : node -> Address.t
 (** Constructs a [Peer.t] from a [Types.node]. *)
 val peer_from : node -> Peer.t
 
-(** Add a peer to the know peers. *)
+(** Add a peer to the known peers. *)
 val add_peer : node -> Peer.t -> [`Duplicate | `Ok]
+
+(** Begins disseminating an encoded message meant to be witnessed by as many
+    nodes in the network as possible. *)
+val post : node ref -> Message.t -> unit
 
 (** [create_request node recipient payload] creates a [Message.t] of the {i Request category}
 addressed to {i recipient} containing {i payload}. *)
@@ -22,13 +26,6 @@ val create_request : node ref -> Address.t -> bytes -> Message.t Lwt.t
 that responds to {i request} whose content is {i payload}. *)
 val create_response : node ref -> Message.t -> bytes -> Message.t
 
-(** Sends a message via datagram from the given [Types.node]
-to a specified peer within the [Message.t]. Construct a message with one of the
-[create_*] functions to then feed to this function. *)
-val send_to : node ref -> Message.t -> unit Lwt.t
-
-(** Waits for the next incoming message and returns it. *)
-val recv_next : node ref -> Message.t Lwt.t
 
 (** Sends an encoded {i request} to the specified peer and
 returns a promise holding the response from the peer. This
@@ -40,3 +37,5 @@ val request : node ref -> bytes -> Address.t -> Message.t Lwt.t
 of recipients and collects the responses in a list of [Message.t Lwt.t]. *)
 val broadcast_request :
   node ref -> bytes -> Address.t list -> Message.t Lwt.t list
+
+val create_post : node ref -> bytes -> Message.t
