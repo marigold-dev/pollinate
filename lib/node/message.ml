9e0e84d107a02019a4395be634d9ff8523e74bad
Ambika Eshwar <ambika@rosalogia.me>
2022-05-27 14:59:24 -0500

    Update messages for dissemination
    * Add post category
    * Derive show for debugging purposes
    * Add timestamp for checking if message is out of date
    * Make recipients an Address.t list so that messages can be constructed without recipients, then have recipients added later on
    * Add message hashing for easily checking if a message has been seen
      before

--- lib/node/message.ml
+++ lib/node/message.ml
@@ -5,16 +5,30 @@ type category =
   | Uncategorized
   | Request
   | Response
+  | Post
   | Failure_detection
   | Custom            of string
-[@@deriving bin_io]
+[@@deriving bin_io, show]
 
 type t = {
   category : category;
   sub_category_opt : (string * string) option;
   id : int;
+  timestamp : float;
   sender : Address.t;
-  recipient : Address.t;
+  recipients : Address.t list;
   payload : bytes;
 }
 [@@deriving bin_io]
+
+let hash_of m =
+  [
+    m.sender.address;
+    string_of_int m.sender.port;
+    string_of_float m.timestamp;
+    Bytes.to_string m.payload;
+  ]
+  |> String.concat ""
+  |> Digest.string
+  |> Digest.to_hex
+  |> fun s -> String.sub s 0 7
