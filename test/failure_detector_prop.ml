bd5611e8c2188485e15796766a6c186338c5f000
Ambika Eshwar <ambika@rosalogia.me>
2022-06-01 11:35:21 -0500

    Removed knuth shuffle and pick random peers from Failure detector

--- test/failure_detector_prop.ml
+++ test/failure_detector_prop.ml
@@ -7,11 +7,11 @@ module SUT = Pollinate.Node.Failure_detector
 let node_a =
   Lwt_main.run (Node.init Address.{ address = "127.0.0.1"; port = 3002 })
 
-let knuth_shuffle_size =
+(* let knuth_shuffle_size =
   QCheck2.Test.make ~count:1000
     ~name:"Knuth_shuffle does not change the size of the list"
     (QCheck2.Gen.list Generators.peer_gen) (fun peers ->
-      List.length (SUT.knuth_shuffle peers) == List.length peers)
+      List.length (SUT.knuth_shuffle peers) == List.length peers) *)
 
 let update_peer =
   QCheck2.Test.make ~count:1000
@@ -22,7 +22,7 @@ let update_peer =
       let _ = SUT.update_peer_status node_a neighbor neighbor_status in
       neighbor.status = neighbor_status)
 
-let pick_random_neighbors =
+(* let pick_random_neighbors =
   QCheck2.Test.make ~count:1000
     ~name:
       "pick_random_neighbors on a peer with a single neighbor, returns this \
@@ -31,11 +31,14 @@ let pick_random_neighbors =
       let _ = add_neighbor peer neighbor in
       let random_neighbor =
         List.hd @@ SUT.pick_random_neighbors peer.neighbors 1 in
-      random_neighbor == neighbor.address)
+      random_neighbor == neighbor.address) *)
 
 let () =
   let failure_detector_prop =
     List.map QCheck_alcotest.to_alcotest
-      [knuth_shuffle_size; update_peer; pick_random_neighbors] in
+      [ update_peer
+      (* ; knuth_shuffle_size
+      ; pick_random_neighbors *)
+      ] in
   Alcotest.run "Failure detector"
     [("failure_detector.ml", failure_detector_prop)]
