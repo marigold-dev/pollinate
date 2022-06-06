9b50449fd82ba50ef72592874a1de4d61227c32b
Ambika Eshwar <ambika@rosalogia.me>
2022-06-06 13:35:52 -0500

    Add documentation for Client.create_post

--- lib/node/client.mli
+++ lib/node/client.mli
@@ -32,8 +32,8 @@ val post : node ref -> Message.t -> unit
     entire network. *)
 val post : node ref -> Message.t -> unit
 
-(** Begins disseminating an encoded message meant to be witnessed by as many
-    nodes in the network as possible. *)
+(** Begins disseminating an encoded message meant to be witnessed by the
+    entire network. *)
 val post : node ref -> Message.t -> unit
 
 (** [create_request node recipient payload] creates a [Message.t] of the {i Request category}
